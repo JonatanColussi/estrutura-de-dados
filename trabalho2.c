e17dfd0c3a3d218144ddf23d2dcb0065836c99b3
JonatanColussi <jonata.colussi@teraware.com.br>
2017-05-11 01:07:01 -0300

    Trabalho 2 | inserção

--- trabalho2.c
+++ trabalho2.c
@@ -21,6 +21,7 @@ typedef struct{
 	char nome[30];
 	char endereco[30];
 	char telefone[30];
+	char zona[1];
 } ENTIDADE;
        
 typedef struct LISTA {
@@ -63,15 +64,15 @@ main( void ){
                 break;
            
            case '3':
-                remover_registro(&p);
+                //remover_registro(&p);
                 break;
                                                                             
            case '4':               
-                imprime_lista(p); 
+                //imprime_lista(p); 
                 break;
                 
             case '5':
-                ordena_lista(&p);
+                //ordena_lista(&p);
                 break;
                                 
            case '0':                                           
@@ -90,11 +91,54 @@ main( void ){
         printf( "\n" );
     }
 }
+	
+void entrada_dados(LISTA* aux){
+	printf("\n\n Digite o código: "); 
+    fflush( stdin );// limpa buffer do teclado
+    scanf("%d", &aux->info.codigo);
 
-void entrada_dados(LISTA* aux){}
-void imprime_lista(LISTA* aux){}
-void criar_lista(LISTA** p){}
-void ordena_lista(LISTA** p){}
-void inserir_registro(LISTA** p){}
-LISTA* procura_nodo(LISTA* p, int codigo){}
-void remover_registro(LISTA** p){}
+    printf("\n Digite o nome da entidade: ");
+    fflush(stdin);// limpa buffer do teclado
+    gets(aux->info.nome);
+    
+    printf("\n Digite o endereço da entidade: ");
+    fflush(stdin);// limpa buffer do teclado
+    gets(aux->info.endereco);
+    
+    printf("\n Digite o telefone da entidade: ");
+    fflush(stdin);// limpa buffer do teclado
+    gets(aux->info.telefone);
+    
+    printf("\n Informa a zona [N, S, L, O]: ");
+    do{
+	    fflush(stdin);// limpa buffer do teclado
+	    gets(aux->info.zona);
+	    
+	    if(aux->info.zona[0] != 'N' || aux->info.zona[0] != 'S' || aux->info.zona[0] != 'L' || aux->info.zona[0] != 'O')
+	    	printf("\n\t ZONA INVÁLIDA!");
+	}while(aux->info.zona[0] != 'N' || aux->info.zona[0] != 'S' || aux->info.zona[0] != 'L' || aux->info.zona[0] != 'O');
+  
+    aux->prox= NULL;     // não aponta
+}
+//void imprime_lista(LISTA* aux){}
+void criar_lista(LISTA** p){
+	*p = NULL;
+}
+//void ordena_lista(LISTA** p){}
+void inserir_registro(LISTA** p){
+	LISTA *no = (LISTA *) malloc(sizeof(LISTA));
+    
+    if(no != NULL){
+    	entrada_dados(no);
+    	if(p == NULL)
+    		*p = no;
+    	else{
+    		no->prox = *p;
+    		*p = no;
+		}
+		printf("\n Registro inserido\n");
+	}else
+		printf("\n Lista Cheia \n");
+}
+//LISTA* procura_nodo(LISTA* p, int codigo){}
+//void remover_registro(LISTA** p){}
