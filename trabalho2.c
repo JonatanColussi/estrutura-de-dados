62d450b66aef70f398267cf169f6c2ee297e02d6
Marcus <marcus.medeiros@outlook.com.br>
2017-05-11 17:34:25 -0300

    Ta faltando coisa ainda,deu um bug, ainda nao consegui resolver

--- trabalho2.c
+++ trabalho2.c
@@ -1,3 +1,4 @@
+
 /***********************************************/ 
 /*                                             */
 /* objetivo: trabalho Daniela                  */
@@ -43,7 +44,7 @@ void criar_lista(LISTA** p); // inicializa lista com NULL
 void ordena_lista(LISTA** p); // ordena lista por campo a ser escolhido
 void inserir_registro(LISTA** p); // inclui um novo registro no inicio da lista
 LISTA*  procura_nodo( LISTA* p, int cod ); // procura na lista por uma codigo
-void remover_registro(LISTA** p); // exclui um regitro por codigo 
+void remover_registro(LISTA** l); // exclui um regitro por codigo 
 
 
 /***********************************************/ 
@@ -76,7 +77,7 @@ int main ( void ){
                 break;
            
            case '3':
-                //remover_registro(&p);
+                remover_registro(&p);
                 break;
                                                                             
            case '4':               
@@ -343,4 +344,47 @@ void ordena_lista( LISTA** l )
 }
 }
 
+LISTA* procura_nodo( LISTA *p, int cod ){
+	while(p->info.codigo != cod){ // repete ate encontrar o cod desejado
+		p=p->prox;	// percorre lista
+		if(p==NULL) // toda lista percorrida e nao encontrou
+			return NULL;
+	}
+	return p; 		// encontrou, retorna o endereco do cod desejado
+}
 
+
+
+void remover_registro(LISTA** l){
+	if(*l != NULL){
+		int cod = 0;
+		LISTA *p = *l;
+		LISTA *pc = p;
+		printf("\n\n  Digite o codigo do registro para exclusao: ");
+		fflush(stdin);
+		scanf("%d",&cod);
+		p = procura_nodo(p, cod);
+		
+		if(p != NULL){
+			if(pc==p){
+				*l=p->prox;
+				pc->prox=p->prox;
+				free(p);
+				printf("funcionou");
+			}else{
+				while(pc->prox!=p){
+					pc=pc->prox;
+					pc->prox=p->prox;
+					free(p);
+					p=NULL;
+				printf("Aqui tambem ");
+				}
+			}
+		}else{
+			printf(" Codigo não encontado");
+		}	
+		
+	}else{
+		printf(" Lista Vazia");
+	}
+}
